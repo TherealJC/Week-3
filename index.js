3b9f13a96b749627ff0bafd855bb5e57fa5a798a
unknown <jamie-crowther@live.nmit.ac.nz>
2020-03-09 13:46:02 +1300

    ++x++

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-Operator             
-   +                  Addition
-   -                 Subtraction
-   *                 Multiplication
-   /                  Division
-   %                  Modulus
-  **                 Exponentiation
-
+//Addition Operation Examples
   let x = 10;
   let y = 5;
   console.log(x + y);
+
+console.log(x);
+console.log(++x);
+console.log(x);
+console.log(++x)
+console.log(x);
+console.log(++x)
+console.log(x)
+console.log(++x)
+
+
+
+
+
+
+
+
+
+//   let x = 10;
+//   let y = 5;
+//   let result = x + y;
+//   console.log(result);
+
+//   let x = 10 + 5
+//   console.log(x)
+
+// Increment Operator (++) increments the variable value by one
+// syntax: ++x (or) x++ 
+
+// Pre-Increment: Increase by one, then return x
+// Post-Increment: Returns x, then increase by one
+
+//These next statements are made too difficult to understand, so i really dont know
+// Pre Increment: Increments the value and execute the expression/ statement
+// Post Increment: Evaluate the expression/ statement, then increments the value
+
+// Pre-decrement : Decrements the value by one, then return x
+// Post-decrement : Returns x, then decrements value by one
+
