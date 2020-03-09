a048bca9612ea3b7498768a6249bd0b22951443b
unknown <jamie-crowther@live.nmit.ac.nz>
2020-03-10 09:31:24 +1300

    added operator explanations

--- index.js
+++ index.js
@@ -68,7 +68,32 @@ console.log(10 + 10 + '5');
 console.log('10' + '10');
 //what about
 console.log("10" + "10");
-//Is there no difference ?????
+//No difference
+
+//10/03/2020
+console.log( +true );  // 1, true is converted to 1 using unary plus
+
+console.log( +false ); // 0, false is converted to 0
+
+console.log( +"" );   // 0, empty string converted to 0
+
+console.log(10 + 5 * 3); // 25, First Multiplication then Addition
+
+let x = 10;
+//Assign
+console.log( "=" )
+//Add and Assign (adds x and then assigns original value of x)
+console.log( x += 10);
+//Subtract and Assign
+console.log( x -= 10);
+//Multiply and Assign
+console.log( x *= 10);
+//Divide and Assign
+console.log( x /= 10);
+//Divide and Assign Modulus
+//The modulus assignment operator divides the value stored in the left variable by the right value.
+//If value is a string, an attempt is made to convert the string to a number before performing the modulus and assignment.
+console.log( x %= 9);
 
 
 
